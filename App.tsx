9bc8514d991f74a3eb56315566103fd9e7bdaba3
nikhilsaini2 <nikhilsaini7981@gmail.com>
2026-05-24 17:49:59 +0530

    feat: wire up app routing and assemble App.tsx

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './src/app/store';
+import RootNavigator from './src/navigation/RootStack';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <RootNavigator />
+          
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
+  );
+}
